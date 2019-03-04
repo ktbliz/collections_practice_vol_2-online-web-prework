6d9fb86249b08d3ccb12ddacb99ff74f2a7fa193
Katie Blizzard <kblizzard11@gmail.com>
2019-03-04 15:14:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,69 @@
-# your code goes here
+def begins_with_r(array)
+  new_array = []
+  array.each do |element|
+    new_array << element.start_with?("r")
+  end
+  if new_array.include?(false)
+    return false
+  else
+    return true 
+  end 
+end 
+
+
+def contain_a(array) 
+  new_array = []
+  array.each do |element|
+    if element.include?("a")
+        new_array << element
+    end
+  end
+  new_array
+end 
+
+
+def first_wa(array)
+  array.find do |element|
+    if element.class == String 
+      element.start_with?("wa")
+    end
+  end
+end 
+
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    element.class != String 
+  end 
+end 
+
+
+def count_elements(array) 
+  new_array = []
+  array.collect do |element|
+    count = array.count(element)
+    new_array << {:count => count, :name => element[:name]}
+  end
+  new_array.uniq 
+end 
+
+
+def merge_data(one, two)
+  
+  one.each do |x|
+    
+  
+end 
+
+
+def find_cool
+
+end 
+
+
+def organize_schools
+
+end 
+
+
+
